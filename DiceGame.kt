1cc5340c8b92dd9364381bb4f083bae29ef313f3
Zachary <zdean1995@gmail.com>
2023-02-17 20:21:50 -0500

    Finished Assignment

--- DiceGame.kt
+++ DiceGame.kt
@@ -1,9 +1,20 @@
+/*
+Zachary Dean
+A00275392
+JAV-1001 - 11354
+
+This is a runner console application for testing the Die class.
+ */
+
 fun main() {
+    //creates 3 dice, one with each constructor
     println("creating 3 dice using all 3 constructors")
     val die1 = Die()
     val die2 = Die(30)
-    val die3 = Die("Coin", 2)
+    val die3 = Die("Coin", 2) //a coin is basically a 2 sided die, right?
 
+
+    //rolling all three die
     println("${die1.name} side up before rolling is ${die1.currentSideUp}")
     println("${die2.name} side up before rolling is ${die2.currentSideUp}")
     println("${die3.name} side up before rolling is ${die3.currentSideUp}")
@@ -16,9 +27,12 @@ fun main() {
     println("${die2.name} side up after rolling is ${die2.currentSideUp}")
     println("${die3.name} side up after rolling is ${die3.currentSideUp}")
 
+    //setting the die made with the two argument constructor to it's highest roll amount
     die3.currentSideUp = die3.sides
     println("${die3.name} has been set to it's highest value, which is ${die3.currentSideUp}")
 
+
+    //Playing yahtzee with five d6s.  I used a do while loop so it would roll them all at least once in the case that all five start with the same value
     println("Playing Yahtzee")
     var rollCount = 0
     println("Creating 5 d6s")
--- Die.kt
+++ Die.kt
@@ -1,30 +1,42 @@
+/*
+Zachary Dean
+A00275392
+JAV-1001 - 11354
+
+This is the die class, a class that is used to represent phyiscal dice
+ */
+
 import kotlin.random.Random
 
+//The class itself, I've decided to make teh name and sides unchangeable since you can't change the number of sides on a die
 class Die {
     val name: String
     val sides: Int
     var currentSideUp = 0
 
+    //zero argument constructor sets the name to d6 and the sides count to 6.  All constructors use the roll function to get the initial side up
     constructor() {
         name = "d6"
         sides = 6
         roll()
     }
 
+    //one argument constructor takes in the number of sides as a parameter and sets the name to "d" plus that number
     constructor(_sides: Int) {
         sides = _sides
         name = "d$sides"
         roll()
     }
 
+    //two argument constructor takes in both the name and the number of sides
     constructor(_name: String, _sides: Int) {
         name = _name
         sides = _sides
         roll()
     }
 
+
     fun roll() {
         currentSideUp = Random.nextInt(1, sides)
-        //println("Your $name rolled a $currentSideUp!")
     }
 }
